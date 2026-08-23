48c31cc9feae26a684ed1dbe545b2b04d524d75f
Haha452123 <Christianaull@outlook.de>
2026-08-23 00:16:27 -0500

    Export firebaseConfig for external use

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   "apiKey": "AIzaSyAcQu7U91BAoVuVdCzFrVfqvzGK7iyJnjA",
   "authDomain": "p9w-26379.firebaseapp.com",
   "databaseURL": "https://p9w-26379-default-rtdb.firebaseio.com",
