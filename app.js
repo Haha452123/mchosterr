4999bc3074a1c04bc0569341e23d9a44fc9faec4
Haha452123 <Christianaull@outlook.de>
2026-08-29 22:01:57 -0500

    Update admin UID placeholder in app.js
    Replace placeholder for admin UID in the configuration.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const FREE_RAM_LIMIT = 3072;
 
 // Put your Firebase Auth UID here if you want admin access.
 const ADMIN_UIDS = [
-""
+"REPLACE_WITH_ADMIN_UID"
 ];
 
 const MODRINTH_API = "https://api.modrinth.com/v2";
